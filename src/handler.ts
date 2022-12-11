f629bcf8fecc40e7166c55c1352e2da403bfc22d
James <james@realja.me>
2022-12-10 23:02:30 -0500

    Make todo function for converting PostRaw into Post object

--- src/handler.ts
+++ src/handler.ts
@@ -2,16 +2,19 @@
 
 import { redditFetch } from "./fetch";
 
+async function createPostObject(dataRaw: PostRaw) {
+	// Populates a final Post object with necessary data from the JSON fetched from Reddit.
+	// TODO
+}
+
 export const handleRequest = async (url: string) => {
 	console.log("Handling request");
 
 	const data = await redditFetch(url);
 
-	const metaTags = [];
+	const metaTags = [""];
 
 	// Add metadata relating to the author and body text.
-	metaTags.push(`<meta property="og:description" content="Hello World" />`);
-
 	// TODO: parse the fetched data and turn it into meta tags.
 
 	return metaTags.join("");
